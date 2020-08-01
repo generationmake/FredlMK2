f8519902321200cecebc409d9a6bfc6c1f24c487
Bernhard Mayer <bernhard@generationmake.de>
2020-08-01 02:45:31 +0200

    added qr code recognition

--- FredlMK2.ino
+++ FredlMK2.ino
@@ -10,6 +10,24 @@ TMC5130 tmc2(4); // stepper 2
 
 #define EMERGENCYSTOP A4
 
+#include "VidorGraphics.h"
+#include "VidorCamera.h"
+
+VidorCamera vcam;
+
+//#define MAXDIM 10
+
+//static uint16_t x[QR_PT_DET_NUM], y[QR_PT_DET_NUM];
+
+//struct qrPtn {
+//  uint16_t x[QR_PT_DET_NUM];
+//  uint16_t y[QR_PT_DET_NUM];
+//};
+
+//static qrPtn qrBufferPtn[MAXDIM];
+
+//uint16_t count = 0, last;
+
 //returns the input voltage
 float inputvoltage(void)
 {
@@ -27,6 +45,26 @@ void setup() {
   Serial.begin(115200);
   pinMode(EMERGENCYSTOP,  INPUT_PULLDOWN);   // set EMERGENCYSTOP pin to input with pull down
   delay(4000);
+
+  
+  if (!FPGA.begin()) {
+    Serial.println("Initialization failed!");
+    while (1) {}
+  }
+  /**
+    begin() enable the I2C communication and initialize the display for the camera
+  */
+  if (!vcam.begin()) {
+    Serial.println("Camera begin failed");
+    while (1) {}
+  }
+  /**
+      qrrec.begin(); enable the QR code recognition
+  */
+  vcam.qrrec.begin();
+  delay(4000);
+  Serial.println("Power ON");
+  
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
   Serial.println("setup SPI for TMC");
@@ -41,10 +79,42 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("input voltage: ");    
-  Serial.println(inputvoltage());    
+//  Serial.print("input voltage: ");    
+//  Serial.println(inputvoltage());    
+  int validcounter=0;
+  static int qrrecognized=0;
 
-  if(emergencybutton())
+    /**
+     qrrec.readQRCode(); get, if available, the coordinates of the QR code in the screen
+  */
+  vcam.qrrec.readQRCode();
+//  Serial.print("QR_PT_DET_NUM: ");    
+//  Serial.println(QR_PT_DET_NUM);    
+  for (int i = 0; i < QR_PT_DET_NUM; i++) {
+    if (vcam.qrrec.qr.pt[i].valid) {
+      validcounter++;
+//  Serial.println("valid");    
+//      x[i] = (vcam.qrrec.qr.pt[i].xs + vcam.qrrec.qr.pt[i].xe) / 2;
+//      y[i] = (vcam.qrrec.qr.pt[i].ys + vcam.qrrec.qr.pt[i].ye) / 2;
+//      vcam.vgfx.Cross(x[i], y[i], 65535);
+
+    }
+  }
+
+//  last = count % MAXDIM;
+//  for (int i = 0; i < QR_PT_DET_NUM; i++) {
+//    vcam.vgfx.Cross(qrBufferPtn[last].x[i], qrBufferPtn[last].y[i], 0, 0);
+//    qrBufferPtn[last].x[i] = x[i];
+//    qrBufferPtn[last].y[i] = y[i];
+//  }
+//  count++;
+  Serial.println(validcounter);
+  if(validcounter>2) qrrecognized++;
+  else qrrecognized=0;
+  if(qrrecognized>2) Serial.println("QR code recognized!!!");
+  delay(30);    
+
+/*  if(emergencybutton())
   {
     Serial.println("emergency button active");    
   }
@@ -87,6 +157,6 @@ void loop() {
     tmc1.readtstep(); // read tstep
     tmc2.readtstep(); // read tstep
   }
-  delay(4000);
+  delay(4000);*/
 
 }
